82858ab0f17d728f21c8e58dc0488fdb4d2a4a6c
Murilo A. <muriloandradealmeida@gmail.com>
2025-05-21 21:38:36 -0300

    feat: add list card handler and register it on router

---
+++ app/gateways/http/v1/card-brand/list_card_brand.go
@@ -0,0 +1,53 @@
+package cardbrand
+
+import (
+	"context"
+	"log/slog"
+
+	"github.com/danielgtaylor/huma/v2"
+	"github.com/muriiloandrade/finsplitter/app/domain/entity"
+	"github.com/muriiloandrade/finsplitter/app/domain/usecases"
+
+	slogctx "github.com/veqryn/slog-context"
+)
+
+const operation = "handler.ListCardBrands"
+
+type ListCardBrandsRequest struct{}
+
+type ListCardBrandsResponse struct {
+	Body struct {
+		CardBrands []entity.CardBrand `json:"data"`
+		TotalCount int                `json:"total_count"`
+	}
+}
+
+type ListCardBrandsHandler struct {
+	useCase usecases.ListCardBrandUC
+}
+
+func NewListCardBrandsHandler(useCase usecases.ListCardBrandUC) ListCardBrandsHandler {
+	return ListCardBrandsHandler{
+		useCase: useCase,
+	}
+}
+
+func (h ListCardBrandsHandler) ListCardBrands(ctx context.Context, input *ListCardBrandsRequest) (*ListCardBrandsResponse, error) {
+	logger := slogctx.FromCtx(ctx)
+
+	cardBrands, err := h.useCase.ListCardBrands(ctx)
+	if err != nil {
+		logger.Error("Failed to list card brands", slog.String("operation", operation), slog.Any("error", err))
+		return nil, huma.Error500InternalServerError(err.Error())
+	}
+
+	return &ListCardBrandsResponse{
+		Body: struct {
+			CardBrands []entity.CardBrand `json:"data"`
+			TotalCount int                `json:"total_count"`
+		}{
+			CardBrands: cardBrands,
+			TotalCount: len(cardBrands),
+		},
+	}, nil
+}
