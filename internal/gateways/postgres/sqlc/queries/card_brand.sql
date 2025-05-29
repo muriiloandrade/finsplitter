befa00a57a6fe79843d22ef41fc1a823260a5efb
Murilo A <muriloandradealmeida@gmail.com>
2025-05-29 19:28:08 -0300

    feat: update sqlc generated code

--- internal/gateways/postgres/sqlc/queries/card_brand.sql
+++ internal/gateways/postgres/sqlc/queries/card_brand.sql
@@ -4,6 +4,10 @@ WHERE id = $1 LIMIT 1;
 
 -- name: ListCardBrands :many
 SELECT * FROM card_brand
+WHERE
+  ((sqlc.narg(name)::text IS NULL OR sqlc.narg(name)::text = '') OR name ILIKE '%' || sqlc.narg(name)::text || '%')
+  AND
+  (sqlc.narg(id)::uuid IS NULL OR id = sqlc.narg(id)::uuid)
 ORDER BY name;
 
 -- name: CreateCardBrand :one
@@ -14,6 +18,13 @@ INSERT INTO card_brand (
 )
 RETURNING *;
 
--- name: DeleteCardBrand :exec
+-- name: UpdateCardBrand :one
+UPDATE card_brand
+SET name = $1
+WHERE id = $2
+RETURNING *;
+
+-- name: DeleteCardBrand :one
 DELETE FROM card_brand
-WHERE id = $1;
+WHERE id = $1
+RETURNING *;
