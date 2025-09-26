afec6a182d61a72755e1c803ce342f4869c10c75
Murilo A. <muriloandradealmeida@gmail.com>
2025-09-26 20:43:28 -0300

    refactor: fix gochecknoglobals lint errors

--- internal/gateways/postgres/builder.go
+++ internal/gateways/postgres/builder.go
@@ -2,4 +2,5 @@ package postgres
 
 import "github.com/Masterminds/squirrel"
 
+//nolint:gochecknoglobals // meant to be used on repositories
 var psql = squirrel.StatementBuilder.PlaceholderFormat(squirrel.Dollar)
